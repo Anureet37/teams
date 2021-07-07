f61b2a1e54bd4f6add426593eac5e729bbf1d27e
Anureet Kaur <anureetkaur204@gmail.com>
2021-07-07 17:16:47 +0530

    chat_app

---
+++ video_Call/chat/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgXt1y8xmJGvpEPSbdTXf1B7c38lIILC4",
+    authDomain: "chat-app-372.firebaseapp.com",
+    projectId: "chat-app-372",
+    storageBucket: "chat-app-372.appspot.com",
+    messagingSenderId: "228808271979",
+    appId: "1:228808271979:web:7afade3cb10d2cc76c692e",
+    measurementId: "G-9S588C3ZMD"
+  };
